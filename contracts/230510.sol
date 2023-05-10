3b67a5783b175963835e95bf095dd1d21ddc1d00
dowoo303 <dowoo303@naver.com>
2023-05-10 11:50:15 +0900

    IF_studentScore

--- contracts/230510.sol
+++ contracts/230510.sol
@@ -40,61 +40,6 @@ contract repeat {
 }
 
 contract IF {
-
-    // 점수에 따른 학점 물어보기
-    function setGrade(uint _score) public pure returns(string memory) {     // 모든것을 지역변수로 커버가 가능하기 떄문에 prue
-        if(_score >=50) {
-            return "P";
-        } else {
-            return "F";
-        }
-    }
-
-    // 점수가 70점 이상이면 A, 50점 이상이면 B, 아니면 C반
-    function setGrade2(uint _score) public pure returns(string memory) {
-        if(_score >= 70) {
-            return "A";
-        } 
-        else if(_score >= 50) {
-            return "B";
-        } else {
-            return "C";
-        }
-    }
-
-
-    // 짝수면 even array에, 홀수면 odd array에 넣어보기
-    uint[] even;
-    uint[] odd;
-
-    function evenOrOdd(uint _n) public {        // 상태변수를 변조시킴(public)
-        if(_n % 2 == 0) {
-            even.push(_n);
-        } else {
-            odd.push(_n);
-        }
-    }
-
-    function getEvenAndOdd() public view returns(uint[] memory, uint[] memory) {
-        return (odd, even);
-    }
-
-
-    // 3으로 나누었을 때 나머지가 1이면 A, 2이면 B, 0이면 C
-    uint[] A;
-    uint[] B;
-    uint[] C;
-
-    function ternary(uint _n) public {
-        if(_n % 3 == 1) {
-            A.push(_n);
-        } else if(_n % 2 == 2) {
-            B.push(_n);
-        } else {
-            C.push(_n);
-        }
-    }
-
     struct student {
         uint number;
         string name;
@@ -177,11 +122,17 @@ contract IF {
         Students.push(student(_number, _name, _score, _credit));
     }
 
+    // setGrade 사용해서 간단히 구성해보기
+    function pushStudents2(uint _number, string memory _name, uint _score) public {
+        Students.push(student(_number, _name, _score, setGrade(_score)));
+    }
+
+
     function getStudents() public view returns(student[] memory) {
         return Students;
     }
 
-    function setGrades(uint _score) public pure returns(string memory _credit) {
+    function setGrade(uint _score) public pure returns(string memory _credit) {
         if(_score>= 90) {
             return 'A';
         } else if(_score >= 80) {
