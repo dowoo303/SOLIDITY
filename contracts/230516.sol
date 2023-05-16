cae0266d02b5c7fca8496391f4f25ff2d3b7f190
dowoo303 <dowoo303@naver.com>
2023-05-16 14:40:58 +0900

    Estate_WalletAddress

--- contracts/230516.sol
+++ contracts/230516.sol
@@ -137,136 +137,29 @@ contract DAD {
     uint internal crypto=1000; // 메모장에 private key가 있는 크립토 잔액
     uint private emergency=10000; // 진짜 비상금
 
-    function who() virtual public pure returns(string memory) {
-        return "dad from DAD";
-    }
-
-    function name() internal pure returns(string memory) {
-        return "David";
-    }
-
-    function password() private pure returns(uint) {
-        return 1234;
-    }
-
-    function character() external pure returns(string memory) {
-        return "not good";
-    }
-
-    function getAddress() public view returns(address) {
-        return address(this);       // 이 컨트랙트의 주소
+    function getMsgSender() public view returns(address) {
+        return msg.sender;  // 현재 내 지갑 주소가 나옴
     }
 }
 
 contract MOM {
     DAD husband = new DAD();
-    function who() virtual public pure returns(string memory) {
-        return "mom from MOM";
+    
+    function husGetMsgSender() public view returns(address) {
+        return husband.getMsgSender();      // 현재 지갑 주소가 아닌 MOM 컨트랙트 주소가 나옴
     }
-
-    function getHusbandChar() public view returns(string memory) {
-        return husband.character();
-    }
-
-    function getWallet() public view returns(uint) {
-        return husband.wallet();
-    }
-
-    function husbandAddress() public view returns(address) {
-        return address(husband);
-    }
-
-    function husbandAddress2() public view returns(address) {
-        return husband.getAddress();
-    }
-
-    /*
-    Visibility에서 차단
-    function getCrypto() public view returns(uint) {
-        return husband.crypto();
-    }
-
-    function getEmergency() public view returns(uint) {
-        return husband.emergency();
-    }
-    */
 }
 
 contract CHILD is DAD {
-    function who() override public pure returns(string memory) {
-        return super.who();
-        // return "child from CHILD";
-    }
 
-    function fathersName() public pure returns(string memory) {
-        return super.name();
-    }
-
-    function fathersWallet() public view returns(uint) {
-        return DAD.wallet;      // 상속받은 컨트랙트의 변수 접근은 괄호가 필요없음
-    }
-
-    function fathersCrypto() public view returns(uint) {
-        return DAD.crypto;
-    }
-
-    function fathersAddress() public view returns(address) {
-        return DAD.getAddress();        // 상속받았을 경우 자기 주소
-    }
-
-    function fathersAddress2() public view returns(address) {
-        return super.getAddress();
+    function dadGetMsgSender() public view returns(address) {
+        return super.getMsgSender();    // 현재 내 지갑 주소가 나옴
     }
 
     DAD daddy = new DAD();
     DAD daddy2 = new DAD();
 
-    /*function fathersName2() public pure returns(string memory) {
-        return daddy.name();
-    }*/
-
-    function fathersWallet2() public view returns(uint) {
-        return daddy.wallet();      // 인스턴스화 시킨 변수 접근은 괄호 필요
-    }
-
-    /*function fathersCrypto2() public view returns(uint) {
-        return daddy.crypto();
-    }*/
-
-    function getDaddyAddress() public view returns(address) {
-        return address(daddy);          // daddy 컨트랙트 주소
-    }
-
-    function getDaddyAddress2() public view returns(address) {
-        return daddy.getAddress();      // daddy 컨트랙트 주소
-    }
-
-    function getDaddy2Address() public view returns(address) {
-        return address(daddy2);         // daddy2 컨트랙트 주소
-    }
-
-    function getDaddy2Address2() public view returns(address) {
-        return daddy2.getAddress();     // daddy2 컨트랙트 주소
-    }
-
-    /*
-    Visibility - private 막힘
-    function fathersEmergency() public view returns(uint) {
-        return DAD.emergency;
-    }
-    */
-
-    /*
-    오류 발생 상속받은 아이는 external 접근 불가능
-    function fathersChar() public pure returns(string memory) {
-        return super.character();
-    }
-    */
-
-    /*
-    오류 발생 password는 private 함수
-    function password_Dad() public pure returns(uint) {
-        return super.password();
-    }
-    */
+    function daddyGetMsgSender() public view returns(address) {
+        return daddy.getMsgSender();    // 현재 지갑 주소가 아닌 CHILD 컨트랙트 주소가 나옴
+    }  
 }
