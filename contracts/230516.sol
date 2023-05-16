70da018aca87d178e0cec4183f933cad5ccbbdf5
dowoo303 <dowoo303@naver.com>
2023-05-16 16:25:24 +0900

    EstateAndConstructor

--- contracts/230516.sol
+++ contracts/230516.sol
@@ -132,34 +132,82 @@ contract CHILD1 is DAD1, MOM1 {
 // external은 외부인 접근 or 인스턴스 변수화 시킨걸로만 접근 가능
 contract DAD {
     uint a;
+    address public b;
 
     uint public wallet=100; // 공개한 지갑 잔액
     uint internal crypto=1000; // 메모장에 private key가 있는 크립토 잔액
     uint private emergency=10000; // 진짜 비상금
 
-    function getMsgSender() public view returns(address) {
-        return msg.sender;  // 현재 내 지갑 주소가 나옴
+    function changeWallet(uint _n) internal {
+        wallet = _n;
     }
+
 }
 
 contract MOM {
     DAD husband = new DAD();
+    DAD realHusband;
     
-    function husGetMsgSender() public view returns(address) {
-        return husband.getMsgSender();      // 현재 지갑 주소가 아닌 MOM 컨트랙트 주소가 나옴
+    constructor(address _a) {
+        realHusband = DAD(_a);  // 시작부터 DAD의 지갑주소를 설정하고 시작
+    }
+
+    function getWallet() public view returns(uint) {
+        return husband.wallet();    // DAD의 wallet 값이 변경되도 쫓아가지 못함
+    }
+
+    function getRealWallet() public view returns(uint) {
+        return realHusband.wallet();        // DAD의 wallet 값 변화를 감지할 수 있음
+    }
+
+    function setDad(address _a) public view returns(uint) {
+        DAD copyDad = DAD(_a);
+        return copyDad.wallet();
     }
 }
 
 contract CHILD is DAD {
-
-    function dadGetMsgSender() public view returns(address) {
-        return super.getMsgSender();    // 현재 내 지갑 주소가 나옴
+    function dadChangeWallet(uint _a) public {
+        super.changeWallet(_a);     // 상속받은 자녀도 변수 접근 가능
     }
-
+    
     DAD daddy = new DAD();
     DAD daddy2 = new DAD();
 
-    function daddyGetMsgSender() public view returns(address) {
-        return daddy.getMsgSender();    // 현재 지갑 주소가 아닌 CHILD 컨트랙트 주소가 나옴
-    }  
+     
+}
+
+
+
+
+
+/*
+contract B에서는 새롭게 A를 선언하니 A 선언에 필요한 input 값 2개를 넣어주고
+contract B2에서는 기존의 A를 참조하려고 하니 A 선언에 필요한 input 값 2개가 아닌, contract의 주소를 넣어주어야 함.
+*/
+// 상속+생성자
+contract A {
+    uint public a;
+    uint public b; 
+    constructor(uint _a, uint _b) {
+        a = _a;
+        b = _b;
+    }
+}
+
+contract B {
+    A a = new A(1,2);
+    A b;
+
+    constructor(uint _a, uint _b) {
+        b = new A(_a, _b);
+    }
+}
+
+contract B2 {
+    A b;
+
+    constructor(address _a) {
+        b = A(_a);
+    }
 }
